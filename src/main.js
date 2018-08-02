c7bd07e578066d4d46f44afbdbf551ad5fcc9707
Noah Medoff <noah@>
2018-08-02 11:58:54 -0700

    add working battle on front end

--- src/main.js
+++ src/main.js
@@ -78,6 +78,7 @@ $(document).ready(function(){
       console.log(userCharacter.name);
     })
     $("#start").show();
+    $("#character-creation").hide();
   })
   $("#start").click(function(event){
     event.preventDefault();
@@ -90,32 +91,65 @@ $(document).ready(function(){
   })
   $("#battle").submit(function(event){
     event.preventDefault();
+    let userChanceToHit = Math.floor(Math.random()* 11);
       if ($("#user-action").val() == 1){
         console.log($("#user-action").val());
-        thisBattle.characterOneAction(newAttack);
-        $("#user-result").text("You attacked!" + " and now " + enemyMonster.name + "s health is at " + enemyMonster.hp);
+        if (userChanceToHit >= 3) {
+          thisBattle.characterOneAction(newAttack);
+          $("#user-result").text("You attacked! It did 10 damage!" + " and now " + enemyMonster.name + "s health is at " + enemyMonster.hp);
+
+        } else {
+          $("#user-result").text("You missed! Darn!");
+          $("battle-zone").hide();
+        }
         $("#user-action").hide();
         $("#user-set-action").hide();
         $("#monster-turn").show();
-      } // else if ($("#user-action").val() == 2) {
-      //   console.log($("user-action").val());
-      //   thisBattle.characterOneAction(newDefend);
-      //   $("#user-result").text("You defended!");}
-        else if ($("#user-action").val() == 3) {
-        console.log($("#user-action").val());
-        thisBattle.characterOneAction(newMagic);
+      } else if ($("#user-action").val() == 2) {
+          console.log($("#user-action").val());
+          if (userChanceToHit >= 3) {
+          thisBattle.characterOneAction(newMagic);
+          $("#user-result").text("You used magic! It did 20 damage!" + " and now " + enemyMonster.name + "s health is at " + enemyMonster.hp);
+        } else {
+          $("#user-result").text("You missed! Darn!");
+        }
         $("#user-action").hide();
         $("#user-set-action").hide();
         $("#monster-turn").show();
-        $("#user-result").text("You used magic!" + " and now " + enemyMonster.name + "s health is at " + enemyMonster.hp);
+      }
+      if (enemyMonster.hp <= 0 ) {
+        $("#user-result").text("The enemy is defeated! Good job!")
+        $("battle-zone").hide();
+        $("#monster-turn").hide();
+        $("#monster-result").hide();
+        $("#first-b-one").hide();
       }
   })
   $("#monster-turn").click(function(event){
     event.preventDefault();
     console.log("Monster went!");
     console.log(userCharacter.hp);
-    thisBattle.characterTwoAction(newAttack);
-    $("#monster-result").text(enemyMonster.name + " attacked you! You now have " + userCharacter.hp + " health!");
+    let monsterChoice = Math.floor(Math.random()* 11);
+    if (monsterChoice >= 5){
+      thisBattle.characterTwoAction(newAttack);
+      $("#monster-result").text(enemyMonster.name + " attacked you! It did 10 damage! You now have " + userCharacter.hp + " health!");
+      if (userCharacter.hp <= 0 ) {
+        $("#user-result").text("The enemy defeated you! You lose!")
+      }
+    }
+    else if (monsterChoice <= 3) {
+      $("#monster-result").text(enemyMonster.name + " missed!")
+    } else {
+      thisBattle.characterTwoAction(newMagic);
+      $("#monster-result").text(enemyMonster.name + " used magic on you! It did 20 damage! You now have " + userCharacter.hp + " health!");
+      if (userCharacter.hp <= 0 ) {
+        $("#user-result").text("The enemy defeated you! You lose!")
+        $("battle-zone").hide();
+        $("#monster-turn").hide();
+        $("#monster-result").hide();
+        $("#first-b-one").hide();
+      }
+    }
     $("#monster-turn").hide();
     $("#user-action").show();
     $("#user-set-action").show();
